af18528308da321ef83bc9070b7823ccd66fb985
Jake Vossen <jake@vossen.dev>
2021-11-30 23:21:33 -0700

    AOC day 1

--- Advent of Code 2021/main.swift
+++ Advent of Code 2021/main.swift
@@ -9,3 +9,50 @@ import Foundation
 
 print("Hello, World!")
 
+
+func problem_1() -> UInt {
+    var result: UInt = 0
+    let filename = "input-1.txt"
+    let contents = try! String(contentsOfFile: filename)
+    let lines = contents.split(separator:"\n").map {UInt($0)!}
+    for (i, e) in lines.enumerated().dropFirst(1) {
+        if e > lines[i - 1] {
+            result += 1;
+        }
+    }
+    return result
+
+}
+
+func problem_2() -> UInt {
+    var result: UInt = 0
+    let filename = "input-1.txt"
+    let contents = try! String(contentsOfFile: filename)
+    let nums = contents.split(separator:"\n").map {UInt($0)!}
+    
+//    let chunks: []
+    
+    var chunks: [[UInt]] = []
+    
+    for i in 0...(nums.count - 3) {
+        
+        let new_arr = [nums[i], nums[i+1], nums[i+2]]
+        chunks.append(new_arr)
+        
+    }
+    
+    let sums = chunks.map {$0[0] + $0[1] + $0[2]}
+    
+    for (i, e) in sums.enumerated().dropFirst(1) {
+        if e > sums[i - 1] {
+            result += 1;
+        }
+    }
+    
+    return result
+}
+
+
+print("problem 1: \(problem_1())")
+print("problem 2 \(problem_2())")
+
