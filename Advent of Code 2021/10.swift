c7f8ee4c09299ab416c786e45a6850bc2697ac81
Jake Vossen <jake@vossen.dev>
2021-12-09 23:35:41 -0700

    10 cleanup

--- Advent of Code 2021/10.swift
+++ Advent of Code 2021/10.swift
@@ -8,12 +8,18 @@
 import Foundation
 
 
-func problem_10_1() -> Int {
+// Returns the char count of each bracket (for part 1), then the remainder of the stack if we reach the end (for part 2)
+fileprivate func problem_10_helper() -> ([Character: Int], [[Character]]) {
+    
     let filename = "inputs/input-10.txt"
     let contents = try! String(contentsOfFile: filename)
     let lines = contents.split(separator:"\n").map({Array($0)})
     
     var char_counts: [Character: Int] = [")": 0, "]": 0, "}": 0, ">": 0]
+    
+    
+    var remaining_items: [[Character]] = []
+
     
 lineLoop: for line in lines {
     var stack: [Character] = []
@@ -24,13 +30,13 @@ lineLoop: for line in lines {
             let most_recent = stack.last!
             switch (most_recent, char) {
             case ("(", ")"):
-                let _ = stack.popLast()
+                stack.removeLast()
             case ("[", "]"):
-                let _ = stack.popLast()
+                stack.removeLast()
             case ("{", "}"):
-                let _ = stack.popLast()
+                stack.removeLast()
             case ("<", ">"):
-                let _ = stack.popLast()
+                stack.removeLast()
             case (_, "("), (_, "["), (_, "<"), (_, "{"):
                 stack.append(char)
             default:
@@ -40,58 +46,32 @@ lineLoop: for line in lines {
             
         }
     }
+    remaining_items.append(stack)
 }
+    
+    return (char_counts, remaining_items)
+    
+}
+
+
+func problem_10_1() -> Int {
     var result = 0
     let score_data: [(Character, Int)] = [(")", 3), ("]", 57), ("}", 1197), (">", 25137)]
+    
+    let (char_counts, _) = problem_10_helper()
     
     for (char, mult) in score_data {
         result += char_counts[char]! * mult
     }
-
-    
     return result
     
 }
 
 
 func problem_10_2() -> Int {
-    let filename = "inputs/input-10.txt"
-    let contents = try! String(contentsOfFile: filename)
-    let lines = contents.split(separator:"\n").map({Array($0)})
     
     let score_char: [Character: Int] = ["(": 1, "[": 2, "{": 3, "<": 4]
-    
-    
-    var remaining_items: [[Character]] = []
-    
-lineLoop: for line in lines {
-    var stack: [Character] = []
-    for char in line {
-        if stack.isEmpty {
-            stack.append(char)
-        } else {
-            let most_recent = stack.last!
-            switch (most_recent, char) {
-            case ("(", ")"):
-                let _ = stack.popLast()
-            case ("[", "]"):
-                let _ = stack.popLast()
-            case ("{", "}"):
-                let _ = stack.popLast()
-            case ("<", ">"):
-                let _ = stack.popLast()
-            case (_, "("), (_, "["), (_, "<"), (_, "{"):
-                stack.append(char)
-            default:
-                continue lineLoop
-            }
-            
-        }
-    }
-    
-    remaining_items.append(stack)
-    
-}
+    let (_, remaining_items) = problem_10_helper()
     
     var scores: [Int] = []
     for remaining_item in remaining_items {
